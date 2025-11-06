2b835743b6ede12c5f05bd474206db5f41cc416a
fancyman00 <aleop00040005@gmail.com>
2025-11-06 16:04:36 +0300

    Исправление: ряд теперь передается как параметр
    Ряд передается как предикат SeriesPred(Index, Value) вместо
    захардкоженного ряда натуральных чисел. Это позволяет использовать
    любые ряды: натуральные числа, квадраты, арифметические прогрессии и т.д.
    
    Исправление по комментарию ревьюера.

--- group-9/DombrovskiyAlexey/task-1_op-16/task.pl
+++ group-9/DombrovskiyAlexey/task-1_op-16/task.pl
@@ -1,17 +1,38 @@
 % Номер 1, задача 16
 % Сгенерировать список, содержащий значения первых N элементов указанного ряда.
-% Генерируем первые N натуральных чисел: 1, 2, 3, ..., N
 
-% Основной предикат: генерирует список первых N натуральных чисел
-generate_series(N, List) :-
+% Основной предикат: генерирует список первых N элементов ряда
+% N - количество элементов
+% SeriesPred - предикат для генерации элементов (SeriesPred(Index, Value))
+% List - результирующий список
+generate_series(N, SeriesPred, List) :-
     N > 0,
-    generate_series_helper(1, N, List).
+    generate_series_helper(1, N, SeriesPred, List).
 
 % Вспомогательный предикат для генерации списка
-generate_series_helper(Current, N, [Current]) :-
-    Current =:= N.
+generate_series_helper(Current, N, SeriesPred, [Value]) :-
+    Current =:= N,
+    call(SeriesPred, Current, Value).
 
-generate_series_helper(Current, N, [Current | Rest]) :-
+generate_series_helper(Current, N, SeriesPred, [Value | Rest]) :-
     Current < N,
+    call(SeriesPred, Current, Value),
     Next is Current + 1,
-    generate_series_helper(Next, N, Rest).
+    generate_series_helper(Next, N, SeriesPred, Rest).
+
+
+% Натуральные числа: 1, 2, 3, ...
+natural_number(I, I).
+
+% Квадраты: 1, 4, 9, 16, ...
+square(I, Value) :-
+    Value is I * I.
+
+% Арифметическая прогрессия: a, a+d, a+2d, ...
+arithmetic_progression(Start, Step, I, Value) :-
+    Value is Start + (I - 1) * Step.
+
+% Примеры использования:
+% generate_series(5, natural_number, L).  % L = [1, 2, 3, 4, 5]
+% generate_series(5, square, L).          % L = [1, 4, 9, 16, 25]
+% generate_series(5, arithmetic_progression(2, 3), L).  % L = [2, 5, 8, 11, 14]
