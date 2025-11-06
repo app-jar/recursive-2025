bb233c31c03afbca7546c60a526831e3a04eb51f
Антон Телебоков <ForeverButNotAlone@yandex.ru>
2025-11-06 15:21:27 +0300

    Задача 1 Пролог. 9. Получить список, содержащий все элементы исходного списка, но по одному.

--- group-9/TelebokovAnton/task-1PROLOG_op-9/l1.pl
+++ group-9/TelebokovAnton/task-1PROLOG_op-9/l1.pl
@@ -3,9 +3,13 @@
 unique_elements([], []).
 
 unique_elements([H|T], Result) :-
-    member(H, T),
+    my_member(H, T),
     unique_elements(T, Result).
 
 unique_elements([H|T], [H|Result]) :-
-    \+ member(H, T),
+    \+ my_member(H, T),
     unique_elements(T, Result).
+
+%Дописал свой member
+my_member(X, [X|_]).
+my_member(X, [_|T]) :- my_member(X, T).
