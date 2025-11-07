52abef144655a60739fb40d4373895a39e47f88b
Александр <79480843+laZuR1s@users.noreply.github.com>
2025-11-07 22:30:26 +0300

    Номер 1 задача 19

---
+++ group-9/LazarevAlexandr/task-1_op-19/task.pl
@@ -0,0 +1,8 @@
+partial_sums(List, Sums) :-
+    partial_sums(List, 0, Sums).
+
+partial_sums([], _, []).
+
+partial_sums([H|T], Acc, [NewSum|Sums]) :-
+    NewSum is Acc + H,
+    partial_sums(T, NewSum, Sums).
