c68b9380cd80a4cb093ff57c85177ca7fc7d5402
Александр <79480843+laZuR1s@users.noreply.github.com>
2025-11-09 14:14:53 +0300

    Сделал не ту задачу два месяца назад, my bad

--- group-9/LazarevAlexandr/task-1_op-19/task.pl
+++
@@ -1,9 +0,0 @@
-% Номер 1 задача 19
-partial_sums(List, Sums) :-
-    partial_sums(List, 0, Sums).
-
-partial_sums([], _, []).
-
-partial_sums([H|T], Acc, [NewSum|Sums]) :-
-    NewSum is Acc + H,
-    partial_sums(T, NewSum, Sums).
