a90d87f73ad8a97ea6794b5b57833117b5a3eb17
Ярослава <aroslavakorobova89540@gmail.com>
2025-11-12 22:10:58 +0300

    Добавлено измерение длины

--- group-9/KorobovaYaroslava/Prolog_task-1_op-13/Laba1.pl
+++ group-9/KorobovaYaroslava/Prolog_task-1_op-13/Laba1.pl
@@ -1,28 +1,30 @@
-% 13.Проверить, является ли список повторением одного и того же списка N раз
+% 13. Проверить, является ли список повторением одного и того же списка N раз
 
 % Пустой список - 0 повторений любого шаблона
 repeats([], _, 0).
 
 % Обработка пустого базового списка
-repeats(List, Base, N) :-
-    Base = [], !,  % Обработка пустого Base
-    length(List, N).
+repeats(List, [], N) :-
+    length(List, Len),         
+    (Len = 0 -> N = 0 ; fail).  
 
 % Проверка непустого базового списка
 repeats(List, Base, N) :-
     Base \= [],
-    length(Base, Len),
-    length(List, TotalLen),
+    length(Base, Len),     
+    length(List, TotalLen),  
     TotalLen mod Len =:= 0,
     N is TotalLen // Len,
-    check_repeats(List, Base, N).
+    check_repeats(List, Base, N, Len).  
 
 % Проверка что список состоит из N копий Base
-check_repeats([], _, _).
+check_repeats([], _, 0, _).
 
 % Рекурсивная проверка каждой копии Base
-check_repeats(List, Base, N) :-
+check_repeats(List, Base, N, Len) :-
     N > 0,
-    append(Base, Rest, List),
+    length(Prefix, Len),      
+    append(Prefix, Rest, List),
+    Prefix = Base,          
     N1 is N - 1,
-    check_repeats(Rest, Base, N1).
+    check_repeats(Rest, Base, N1, Len).
