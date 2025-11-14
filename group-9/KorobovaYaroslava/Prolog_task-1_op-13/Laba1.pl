e6381f754ba7701bc99d8b57f88a9ab377443a51
Ярослава <aroslavakorobova89540@gmail.com>
2025-11-14 17:36:34 +0300

    Добавлен length и append

--- group-9/KorobovaYaroslava/Prolog_task-1_op-13/Laba1.pl
+++ group-9/KorobovaYaroslava/Prolog_task-1_op-13/Laba1.pl
@@ -1,21 +1,32 @@
 % 13. Проверить, является ли список повторением одного и того же списка N раз
 
+% Собственная реализация length
+my_length([], 0).
+my_length([_|Tail], Len) :-
+    my_length(Tail, TailLen),
+    Len is TailLen + 1.
+
+% Собственная реализация append
+my_append([], List, List).
+my_append([Head|Tail1], List2, [Head|Tail3]) :-
+    my_append(Tail1, List2, Tail3).
+
 % Пустой список - 0 повторений любого шаблона
 repeats([], _, 0).
 
 % Обработка пустого базового списка
 repeats(List, [], N) :-
-    length(List, Len),         
-    (Len = 0 -> N = 0 ; fail).  
+    my_length(List, Len),
+    (Len = 0 -> N = 0 ; fail).
 
 % Проверка непустого базового списка
 repeats(List, Base, N) :-
     Base \= [],
-    length(Base, Len),     
-    length(List, TotalLen),  
+    my_length(Base, Len),
+    my_length(List, TotalLen),
     TotalLen mod Len =:= 0,
     N is TotalLen // Len,
-    check_repeats(List, Base, N, Len).  
+    check_repeats(List, Base, N, Len).
 
 % Проверка что список состоит из N копий Base
 check_repeats([], _, 0, _).
@@ -23,8 +34,14 @@ check_repeats([], _, 0, _).
 % Рекурсивная проверка каждой копии Base
 check_repeats(List, Base, N, Len) :-
     N > 0,
-    length(Prefix, Len),      
-    append(Prefix, Rest, List),
-    Prefix = Base,          
+    get_prefix(List, Len, Prefix, Rest),
+    Prefix = Base,
     N1 is N - 1,
     check_repeats(Rest, Base, N1, Len).
+
+% Получить префикс определенной длины
+get_prefix(List, 0, [], List).
+get_prefix([Head|Tail], Len, [Head|Prefix], Rest) :-
+    Len > 0,
+    Len1 is Len - 1,
+    get_prefix(Tail, Len1, Prefix, Rest).
