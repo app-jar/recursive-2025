f84d034f32ac5097791d5b8dd10f57f1603784ca
SergeevSergey17484 <podgdmxmdhdmdgs.com>
2025-11-19 18:14:14 +0300

    Номер 1 Prolog, задача 15: Определить наиболее редко встречающееся число в списке

---
+++ group-9/SergeevSergey/task-1Prolog_op-15.pl
@@ -0,0 +1,15 @@
+% 15. Найти наименее частые элементы в списке
+least_frequent(List, Least) :-
+    List \= [],
+    findall(Count-Element, (my_member(Element, List), count(Element, List, Count)), Pairs),
+    min_member(MinCount-_, Pairs),
+    findall(X, my_member(MinCount-X, Pairs), Least).
+
+% Самописный member
+my_member(X, [X|_]).
+my_member(X, [_|T]) :- my_member(X, T).
+
+% Подсчет вхождений элемента
+count(X, List, Count) :-
+    findall(X, my_member(X, List), Occurrences),
+    length(Occurrences, Count). 
