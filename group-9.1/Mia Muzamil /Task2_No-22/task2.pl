2dc83fc04522dd9c88e64ea0502810f1b93ddee6
Muzamil Mia <103633759+MuzamilMia@users.noreply.github.com>
2025-12-02 16:27:12 +0300

    for sign handling and absolute values

--- group-9.1/Mia Muzamil /Task2_No-22/task2.pl
+++ group-9.1/Mia Muzamil /Task2_No-22/task2.pl
@@ -1,33 +1,43 @@
 % Эмуляция деления в столбик рекурсивным способом
 
-% long_div(Dividend, Divisor, Quotient, Remainder).
 
-% Главный предикат:
 long_div(Dividend, Divisor, Quotient, Remainder) :-
-    number_digits(Dividend, Digits),     
-    divide_digits(Digits, Divisor, 0, [], QDigits, Remainder),
-    digits_number(QDigits, Quotient).    % превращаем цифры частного в число
+    % Обработка знаков
+    (Dividend >= 0 -> SignD = 1 ; SignD = -1),
+    (Divisor  >= 0 -> SignV = 1 ; SignV = -1),
 
-% Деление по цифрам:
-% divide_digits(СписокЦифр, Делитель, ТекущийОстаток, АккумуляторЦифрЧастного, РезультатЧастного, ИтоговыйОстаток)
+    % Работаем с абсолютными значениями
+    AbsD is abs(Dividend),
+    AbsV is abs(Divisor),
+    number_digits(AbsD, Digits),
+    divide_digits(Digits, AbsV, 0, [], QDigits, RemainderAbs),
+    digits_number(QDigits, QAbs),
+    QuotSign is SignD * SignV,
+    Quotient is QAbs * QuotSign,
 
-divide_digits([], _, Remainder, Q, Q, Remainder).  
+    Remainder is RemainderAbs * SignD.
+
+% База рекурсии: цифры закончились
+divide_digits([], _, Remainder, Q, Q, Remainder).
+
+% Рекурсивное извлечение цифр
 divide_digits([D|Ds], Divisor, Current, QAcc, Q, Remainder) :-
-    New is Current * 10 + D,            % подносим цифру "вниз"
-    Digit is New // Divisor,            % цифра частного
-    Rem is New mod Divisor,             % новый остаток
+    New is Current * 10 + D,     
+    Digit is New // Divisor,    
+    Rem is New mod Divisor,      
     divide_digits(Ds, Divisor, Rem, [Digit|QAcc], Q, Remainder).
 
+% Преобразования числа и списка цифр
 
 number_digits(N, Digits) :-
     number_chars(N, Cs),
     maplist(atom_number, Cs, Digits).
 
 digits_number(Ds, N) :-
-    reverse(Ds, R),      % разворачиваем, т.к. накапливали в обратном порядке
+    reverse(Ds, R),                  
     digits_number_(R, 0, N).
 
-digits_number_([], Acc, Acc).
+digits_number_([], Acc, Acc).        
 digits_number_([D|Ds], Acc, N) :-
-    NewAcc is Acc * 10 + D,
+    NewAcc is Acc * 10 + D,         
     digits_number_(Ds, NewAcc, N).
