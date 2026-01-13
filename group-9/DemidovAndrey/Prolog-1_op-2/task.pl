b7fc0aeb46160790312ea5e5539a24021eee1d09
Sea.P_D.A <49237780+Sea-P-D-A@users.noreply.github.com>
2026-01-13 23:06:41 +0300

    Update task.pl

--- group-9/DemidovAndrey/Prolog-1_op-2/task.pl
+++ group-9/DemidovAndrey/Prolog-1_op-2/task.pl
@@ -1,7 +1,23 @@
 % Основной предикат: получить список простых чисел до N
 primes_up_to(N, Primes) :-
-    numlist(2, N, Numbers),      % Создаем список чисел от 2 до N
-    include(prime, Numbers, Primes). % Фильтруем, оставляя только простые
+    generate_numbers(2, N, Numbers),  % Создаем список чисел от 2 до N
+    filter_primes(Numbers, Primes).   % Фильтруем, оставляя только простые
+
+% Генерация списка чисел от Start до End
+generate_numbers(Start, End, []) :- Start > End, !.
+generate_numbers(Start, End, [Start|Rest]) :-
+    Start =< End,
+    Next is Start + 1,
+    generate_numbers(Next, End, Rest).
+
+% Фильтрация простых чисел
+filter_primes([], []).
+filter_primes([H|T], [H|Result]) :-
+    prime(H),
+    !,
+    filter_primes(T, Result).
+filter_primes([_|T], Result) :-
+    filter_primes(T, Result).
 
 % Проверка, является ли число простым
 prime(2).                        % 2 - простое число
@@ -9,7 +25,7 @@ prime(3).                        % 3 - простое число
 prime(N) :-
     integer(N), N > 3, 
     N mod 2 =\= 0,               % Простые числа (кроме 2) - нечетные
-    not(has_divisor(N, 3)).      % Проверяем делители начиная с 3
+    \+ has_divisor(N, 3).        % Проверяем делители начиная с 3
 
 % Проверка наличия делителей
 has_divisor(N, D) :-
@@ -19,5 +35,11 @@ has_divisor(N, D) :-
     D2 is D + 2,                 % Переходим к следующему нечетному делителю
     has_divisor(N, D2)).
 
-% Запрос
-primes_up_to(100, Primes).
+% Основная цель для запуска с выводом
+main :-
+    primes_up_to(100, Primes),
+    write('Простые числа до 100: '), write(Primes), nl,
+    length(Primes, Count),
+    write('Количество: '), write(Count), nl.
+
+:- initialization(main).
