a2c58a74dbb1458cad1c612abd1d80c9e381ed88
Sea.P_D.A <49237780+Sea-P-D-A@users.noreply.github.com>
2026-01-13 22:36:37 +0300

    Update task.pl

--- group-9/DemidovAndrey /Prolog-1_op-2/task.pl
+++ group-9/DemidovAndrey /Prolog-1_op-2/task.pl
@@ -1,23 +1,31 @@
 % Основной предикат: получить список простых чисел до N
 primes_up_to(N, Primes) :-
-    numlist(2, N, Numbers),      % Создаем список чисел от 2 до N
-    include(prime, Numbers, Primes). % Фильтруем, оставляя только простые
+    numlist(2, N, Numbers),
+    include(prime, Numbers, Primes).
 
 % Проверка, является ли число простым
-prime(2).                        % 2 - простое число
-prime(3).                        % 3 - простое число
+prime(2).
+prime(3).
 prime(N) :-
-    integer(N), N > 3, 
-    N mod 2 =\= 0,               % Простые числа (кроме 2) - нечетные
-    not(has_divisor(N, 3)).      % Проверяем делители начиная с 3
+    integer(N), N > 3,
+    N mod 2 =\= 0,
+    not(has_divisor(N, 3)).
 
 % Проверка наличия делителей
 has_divisor(N, D) :-
-    D * D =< N,                  % Проверяем делители только до sqrt(N)
-    (N mod D =:= 0               % Если делится без остатка - составное
-    ; 
-    D2 is D + 2,                 % Переходим к следующему нечетному делителю
+    D * D =< N,
+    (N mod D =:= 0
+    ;
+    D2 is D + 2,
     has_divisor(N, D2)).
 
-% Запрос
-primes_up_to(100, Primes).
+% Основной код с выводом
+main :-
+    primes_up_to(100, Primes),
+    write('Простые числа до 100:'), nl,
+    write(Primes), nl,
+    length(Primes, Count),
+    write('Количество простых чисел: '), write(Count), nl.
+
+% Запуск программы
+:- initialization(main).
