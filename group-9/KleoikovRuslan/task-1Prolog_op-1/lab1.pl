51982e31e5324c1e8aa9ce18ef65991bc02a4dc7
slaikerrr <tv.slaikerrr@gmail.com>
2026-01-14 18:13:42 +0300

    Номер 1 Prolog, задача 1 Получить список всех делителей указанного числа.

--- group-9/KleoikovRuslan/task-1Prolog_op-1/lab1.pl
+++ group-9/KleoikovRuslan/task-1Prolog_op-1/lab1.pl
@@ -1,1 +1,14 @@
+%1. Получить список всех делителей указанного числа.
 
+%поиск делителей
+divisors_eff(N, Divisors) :-
+    N > 0,
+    Limit is integer(sqrt(N)),
+    findall(D, (between(1, Limit, D), is_divisor(N, D)), SmallDivisors),
+    findall(Partner, (member(D, SmallDivisors), Partner is N // D, Partner \= D), BigDivisors),
+    append(SmallDivisors, BigDivisors, UnsortedDivisors),
+    sort(UnsortedDivisors, Divisors).
+
+%вспомогательный предикат
+is_divisor(N, D) :-
+    0 is N mod D.
